e4bae2797bba4c036900d00e1e00dcac4273db3e
Alex <alex.akten@outlook.com>
2025-01-16 17:26:26 +0000

    new icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main className="bg-zinc-50 text-zinc-800 py-24 px-4 flex flex-col items-center w-screen">
       <section className="w-full max-w-2xl">
-        <h1 className="font-bold text-3xl md:text-5xl">🧠 99 Mental Models</h1>
+        <h1 className="font-bold text-3xl md:text-5xl">💭 99 Mental Models</h1>
         <h2 className="text-lg md:text-2xl mt-2 opacity-50">
           Principles to Guide your Thinking.
         </h2>
